d1b82229848e236825729559c1f1e21ab54236a3
Matt Heidebrecht <matt@matthewheidebrechtllc.com>
2020-09-11 08:09:46 -0600

    simulation architecture

--- physics_equations.py
+++ physics_equations.py
@@ -6,6 +6,6 @@ def drag_force(coefficient_drag, velocity, air_density, frontal_area):
     return 0.5*air_density*(velocity ** 2) * coefficient_drag * frontal_area
 
 
-# Kinetic energy change from velocity_start to velocity_end of and object with mass 
+# Kinetic energy change from velocity_start to velocity_end of and object with mass
 def kinetic_energy_change(velocity_end, velocity_start, mass):
     return 0.5*mass*(velocity_end ** 2 - velocity_start ** 2)
