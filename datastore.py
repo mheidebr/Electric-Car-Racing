ffcc6fcba75b3caf24ccee66b476901803cf22cb
matt heidebrecht <matt@matthewheidebrechtllc.com>
2020-10-21 08:00:46 -0600

    added thread safe getters and setters for datastore

--- datastore.py
+++ datastore.py
@@ -15,89 +15,119 @@ class DataStore:
 
     def __init__(self):
 
-        self.cars = electric_car_properties.ElectricCarProperties()
+        self._car = electric_car_properties.ElectricCarProperties()
         self._track_profile = track_properties.TrackProperties()
         self._lap_simulation_results = LapVelocitySimulationResults()
+        self._race_simulation_results = RacingSimulationResults()
 
-        # index to do the simulations
+        # simulation time variables
         self._simulation_index = 0
         self._walk_back_counter = 0
         self._velocity = 1
 
-        self._simulation_index_lock = threading.Lock()
-        self._walk_back_counter_lock = threading.Lock()
-        self._velocity_lock = threading.Lock()
+        self._car_lock = threading.Lock()
+        self._track_profile_lock = threading.Lock()
+        self._lap_simulation_results_lock = threading.Lock()
+        self._race_simulation_results_lock = threading.Lock()
+        self._simulation_info_lock = threading.Lock()  # lock for simulation time variables
 
+    # Getters and setters for simulation time variables
     def get_simulation_index(self):
-        with self._simulation_index_lock:
+        with self._simulation_info_lock:
             return self._simulation_index
 
     def get_velocity(self):
-        with self._velocity_lock:
+        with self._simulation_info_lock:
             return self._velocity
 
     def get_walk_back_counter(self):
-        with self._walk_back_counter_lock:
+        with self._simulation_info_lock:
             return self._walk_back_counter
 
     def increment_simulation_index(self):
-        with self._simulation_index_lock:
+        with self._simulation_info_lock:
             temp = self._simulation_index
             self._simulation_index += 1
             logger.debug("index updated to {} from {}".format(self._simulation_index,
-                                                            temp))
+                                                              temp))
 
     def decrement_simulation_index(self):
-        with self._simulation_index_lock:
-            if self._simulation_index < 0:
+        with self._simulation_info_lock:
+            if self._simulation_index > 0:
                 temp = self._simulation_index
                 self._simulation_index -= 1
                 logger.debug("index updated to {} from {}".format(self._simulation_index,
-                                                                temp))
+                                                                  temp))
             # index must be more than or equal to 0
             else:
-                logger.warning("index at {} and decremented, not allowed".format(self._simulation_index))
+                logger.warning("index at {} and decremented, not allowed"
+                               .format(self._simulation_index))
 
     def increment_walk_back_counter(self):
-        with self._walk_back_counter_lock:
+        with self._simulation_info_lock:
             temp = self._walk_back_counter
             self._walk_back_counter += 1
             logger.debug("walk_back_counter updated to {} from {}".format(self._walk_back_counter,
-                                                                        temp))
+                                                                          temp))
 
     def reset_walk_back_counter(self):
-        with self._walk_back_counter_lock:
+        with self._simulation_info_lock:
             temp = self._walk_back_counter
             self._walk_back_counter = 1
             logger.debug("walk_back_counter updated to {} from {}".format(self._walk_back_counter,
-                                                                        temp))
+                                                                          temp))
 
     def set_velocity(self, new_velocity):
-        if new_velocity >= 0:
-            with self._velocity_lock:
+        with self._simulation_info_lock:
+            if new_velocity >= 0:
                 temp = self._velocity
                 self._velocity = new_velocity
                 logger.debug("velocity updated to {} from {}".format(self._velocity,
-                                                                    temp))
-        else:
-            logger.warning("invalid new_velocity input, must be > 0: {}".format(new_velocity))
+                                                                     temp))
+            else:
+                logger.warning("invalid new_velocity input, must be > 0: {}".format(new_velocity))
+
+    # getters and setters for simulation related classes
+    def get_car_properties(self):
+        with self._car_lock:
+            return self._car
+
+    def get_track_profile(self):
+        with self._track_profile_lock:
+            return self._track_profile
+
+    def get_race_results(self):
+        with self._race_simulation_results_lock:
+            return self._race_simulation_results
+
+    def set_car_properties(self, car_properties):
+        with self._car_lock:
+            self._car = car_properties
+
+    def set_race_results(self, race_results):
+        with self._race_simulation_results_lock:
+            self._race_simulation_results = race_results
+
+    # Getters and setters for the lap_results because there is much more
+    # interaction with this class during the simulation and doing a copy of the data
+    # modifying and then setting it would be very intensive
+    def get_lap_results(self):
+        with self._lap_simulation_results_lock:
+            return self._lap_simulation_results
 
     def initialize_lap_profiles(self, length):
-        self._lap_simulation_results.initialize_profiles(length)
-    
-    def get_lap_velocity_profile():
+        with self._lap_simulation_results_lock:
+            self._lap_simulation_results.initialize_profiles(length)
 
-    def get_lap_time_profile():
-
-    def get_track_distance_profile():
-
+    def add_physics_results_to_lap_results(self, physics_results):
+        with self._lap_simulation_results_lock:
+            self._lap_simulation_results.add_physics_results(physics_results)
 
 
 class RacingSimulationResults():
     def __init__(self):
         self.laps_per_pit_stop = 0
         self.lap_time = 0
-
         self.lap_results = 0
 
 
@@ -167,10 +197,3 @@ class LapVelocitySimulationResults():
                                               physics_results.energy_differential_of_battery)
         self.acceleration_profile[index] = physics_results.acceleration
         self.velocity_profile[index] = physics_results.final_velocity
-        print("Physics Results")
-        print(physics_simulation)
-
-        print("time_profile: {}".format(self.time_profile[index]))
-        print("distance_profile: {}".format(self.distance_profile[index]))
-        print("battery_energy_profile: {}".format(self.battery_energy_profile[index]))
-        time.sleep(0.5)
